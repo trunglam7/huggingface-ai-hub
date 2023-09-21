209ce3320b058c4dcf7981b65e120c089e6a6825
Trung Lam <trunglam107@gmail.com>
2023-09-20 22:05:25 -0700

    added multiple image generator

--- app/components/ImgWrapper.tsx
+++ app/components/ImgWrapper.tsx
@@ -3,9 +3,12 @@ import styles from '../styles/imgWrapper.module.css'
 import { ImgWrapperProps } from '@/types'
 
 export default function ImgWrapper({imgUrl} : ImgWrapperProps) {
+
+  const handleDragStart = (e : any) => e.preventDefault();
+
   return (
     <div className={styles.mainContainer}>
-        <img src={imgUrl} alt='generated image'/>
+        <img src={imgUrl} alt='generated image' onDragStart={handleDragStart} role='presentation'/>
     </div>
   )
 }
--- app/components/TextToImage.tsx
+++ app/components/TextToImage.tsx
@@ -1,34 +1,47 @@
-import React, {useEffect, useState} from 'react'
+import React, {useState} from 'react'
 import { HfInference } from '@huggingface/inference'
 import styles from '../styles/textToImage.module.css'
-import { CircularProgress, Button, TextField } from '@mui/material';
+import { CircularProgress, Button, TextField, Select, MenuItem } from '@mui/material';
 
 import ImgWrapper from './ImgWrapper';
 
 export default function TextToImage() {
 
     const [userQuery, setUserQuery] = useState('');
-    const [imageUrl, setImageUrl] = useState('');
     const [generatingImage, setGeneratingImage] = useState(false);
     const [imageReady, setImageReady] = useState(false);
+    const [imageUrlArray, setImageUrlArray] = useState<string[]>([]);
+    const [numImg, setNumImg] = useState(1);
 
     const hf = new HfInference(process.env.NEXT_PUBLIC_HUGGINGFACE_API_KEY);
+
+    const handleSelectChange = (event : any) => {
+        setNumImg(event.target.value);
+    }
 
     const submitQuery = async () => {
         try {
             setGeneratingImage(true);
             setImageReady(false);
 
-            const uniqueUserQuery = userQuery + ' ' + Date.now();
-            const model = await hf.textToImage({
-                inputs: uniqueUserQuery,
-                model: 'stabilityai/stable-diffusion-2',
-                parameters: {
-                  negative_prompt: 'blurry',
-                }
-            });
+            const tempImgArray = [];
+            for(let i = 0; i < numImg; i++){
+                const uniqueUserQuery = userQuery + ' ' + Date.now();
+                const model = await hf.textToImage({
+                    inputs: uniqueUserQuery,
+                    model: 'stabilityai/stable-diffusion-2',
+                    parameters: {
+                    negative_prompt: 'blurry',
+                    }
+                });
 
-            setImageUrl(URL.createObjectURL(model));
+                //setImageUrl(URL.createObjectURL(model));
+                tempImgArray.push(URL.createObjectURL(model));
+                setImageUrlArray([...tempImgArray]);
+                setImageReady(true);
+
+            }
+
         } catch (err) {
             console.log("Error generating image", err);
         } finally {
@@ -42,9 +55,24 @@ export default function TextToImage() {
             <h1>Text to Image Generator</h1>
             <br />
             {generatingImage && <CircularProgress />}
-            {imageReady && <ImgWrapper imgUrl={imageUrl} />}
+            <div className={styles.imageContainer}>
+                {imageReady && imageUrlArray.map(imgUrl => <ImgWrapper imgUrl={imgUrl} />)}
+            </div>
             <div className={styles.inputForm}>
                 <TextField variant='outlined' label='Query' onChange={(e) => setUserQuery(e.target.value)}/>
+                <Select
+                    labelId='number-of-images'
+                    id='number-of-images'
+                    value={numImg}
+                    label='Number of Images'
+                    onChange={handleSelectChange}
+                >
+                    <MenuItem value={1}>1</MenuItem>
+                    <MenuItem value={2}>2</MenuItem>
+                    <MenuItem value={3}>3</MenuItem>
+                    <MenuItem value={4}>4</MenuItem>
+                    <MenuItem value={5}>5</MenuItem>
+                </Select>
                 <Button variant='contained' onClick={submitQuery}>Submit</Button>
             </div>
         </div>
